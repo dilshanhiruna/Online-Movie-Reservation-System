e7de7880bbe61bba71886fa1e4cbf78aef825824
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-04-26 13:47:01 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Research Project Management Tool</h1>
+      <h3>node v16.14.2</h3>
+    </div>
+  );
+}
+
+export default App;
