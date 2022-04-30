5cf3a07f336deba18f689091654dade9bee54922
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-04-30 18:54:19 +0530

    Single Server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./common/database")();
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+//request allow any domain
+app.use(cors({ origin: "*" }));
+
+//Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Route files
+const movies = require("./routes/Movies");
+const reservations = require("./routes/Reservations");
+
+// Mount routers
+app.use("/api/v1/movies", movies);
+app.use("/api/v1/reservations", reservations);
+
+const PORT = 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
