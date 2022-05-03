9ae0ceb99414218c1987c456ba36b97e537bf137
thiyuni <thiyunithilakarathne9848@gmail.com>
2022-05-03 21:59:45 +0530

    Created client folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Customer from './components/Customer';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
+  const user = localStorage.getItem("token");
+  
   return (
     <div className="App">
       <h1>Research Project Management Tool</h1>
       <h3>node v16.14.2</h3>
+
+    <Routes>
+      {user && <Route path="/" exact element={<Customer />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
+
     </div>
   );
 }
