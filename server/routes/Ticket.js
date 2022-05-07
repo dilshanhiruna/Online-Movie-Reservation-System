690ce13c887a764414587ba2171b0bdf04aed4da
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-05-07 20:16:54 +0530

    Removed unwanted files

--- server/routes/Ticket.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createTicket } = require("../controllers/Tickets");
-
-router.post("/", createTicket);
-
-module.exports = router;
