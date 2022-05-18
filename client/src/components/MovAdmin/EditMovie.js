e6812bce192930daa16a663f3a5a0c14d940473b
Tharinda Hashen Kottegoda <hashen@Hashens-MacBook-Air.local>
2022-05-19 00:59:02 +0530

    completed edit function with image upload and preview

--- client/src/components/MovAdmin/EditMovie.js
+++ client/src/components/MovAdmin/EditMovie.js
@@ -8,16 +8,17 @@ import {
   FormControlLabel,
   FormGroup,
   Checkbox,
-} from "@mui/material";
-import { useState, useEffect } from "react";
-import Axios from "axios";
-import { useHistory, useLocation } from "react-router";
+  Grid,
+} from '@mui/material';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { useHistory, useLocation } from 'react-router';
 
 const API = process.env.REACT_APP_API;
 
 export default function EditMovie() {
   const location = useLocation();
-  console.log(location.movie.name);
+  //console.log(location.movie.name);
   const [id, setId] = useState(location.movie._id);
   const [name, setName] = useState(location.movie.name);
   const [description, setDescription] = useState(location.movie.description);
@@ -26,28 +27,31 @@ export default function EditMovie() {
   const [theatersSelected, setTheatersSelected] = useState([]);
   const [showTime, setShowTime] = useState(location.movie.showTime);
   const history = useHistory();
+  const [banner, setBanner] = useState(location.movie.banner);
+  const [selectedImage, setSelectedImage] = useState('');
 
   //get theater list form db
   useEffect(() => {
     Axios.get(`${API}api/v1/theater`)
       .then((res) => {
         setTheatersDB(res.data.data);
-        console.log(theatersDB);
+        //console.log(theatersDB);
         theatersDB.map((theater) => {
-          console.log(theater.theaterName);
+          //console.log(theater.theaterName);
         });
       })
       .catch((err) => {
-        console.log(err);
+        //console.log(err);
       });
   }, []);
   async function onSubmit(e) {
     e.preventDefault();
-    console.log(theatersSelected);
+    //console.log(theatersSelected);
     const movieInfo = {
       name,
       description,
       cast,
+      banner,
       theaters: theatersSelected,
       showTime,
     };
@@ -55,16 +59,16 @@ export default function EditMovie() {
 
     Axios.put(`${API}api/v1/movies/${id}`, movieInfo)
       .then((res) => {
-        alert("Updated");
-        history.push({ pathname: "/movadmin/movies" });
+        alert('Updated');
+        history.push({ pathname: '/movadmin/movies' });
       })
       .catch((err) => {
-        console.log(err);
+        //console.log(err);
       });
   }
 
   const getSelectedTheaters = (value, isChecked) => {
-    // console.log(value + isChecked);
+    // //console.log(value + isChecked);
     if (isChecked) {
       setTheatersSelected((arr) => [...arr, `${value}`]);
     } else {
@@ -75,64 +79,149 @@ export default function EditMovie() {
       }
     }
   };
+
+  //upload image file in cloudinary
+  async function uploadImage(file) {
+    //setting the uploaded image as the selected image
+    const data = new FormData();
+    data.append('file', file);
+    data.append('upload_preset', 'xvtygjxv');
+
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/sliit-hashen/image/upload`,
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    const img = await res.json();
+
+    setSelectedImage(file);
+
+    //setting the saved image url as the banner
+    setBanner(img.secure_url);
+    console.log('url...................' + img.secure_url);
+  }
   return (
     <>
-      <FormControl sx={{ width: "50ch" }}>
-        <TextField
-          id="outlined-basic"
-          label="Movie Name"
-          variant="outlined"
-          onChange={(e) => setName(e.target.value)}
-          defaultValue={name}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Movie Description"
-          variant="outlined"
-          onChange={(e) => setDescription(e.target.value)}
-          defaultValue={description}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Movie Cast"
-          variant="outlined"
-          onChange={(e) => setCast(e.target.value)}
-          defaultValue={cast}
-        />
-
-        <FormGroup>
-          <p id="label">Select Theaters:</p>
-
-          {theatersDB.map((theater) => {
-            return (
-              <FormControlLabel
-                key={theater._id}
-                control={<Checkbox />}
-                label={theater.theaterName}
-                value={theater._id}
-                onChange={(e) =>
-                  getSelectedTheaters(e.target.value, e.target.checked)
-                }
+      <Grid container spacing={10} style={{ paddingLeft: '70px' }}>
+        <Grid item xs={5}>
+          <TextField
+            style={{ width: '100%' }}
+            id="outlined-basic"
+            variant="outlined"
+            onChange={(e) => uploadImage(e.target.files[0])}
+            type="file"
+          />
+          {selectedImage && (
+            <div style={styles.preview}>
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                style={styles.image}
+                alt="Thumb"
               />
-            );
-          })}
-        </FormGroup>
-        <TextField
-          id="outlined-basic"
-          label="Show Time"
-          variant="outlined"
-          onChange={(e) => setShowTime(e.target.value)}
-          defaultValue={showTime}
-        />
-        <br></br>
-        <Button
-          variant="contained"
-          style={{ width: "400px", height: "40px" }}
-          onClick={onSubmit}
-        >
-          Submit
-        </Button>
-      </FormControl>
+            </div>
+          )}
+
+          {selectedImage == '' ? (
+            <img
+              style={{ maxWidth: '100%', maxHeight: '600px' }}
+              src={`${banner}`}
+            />
+          ) : (
+            ''
+          )}
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl sx={{ width: '50ch' }}>
+            <TextField
+              id="outlined-basic"
+              label="Movie Name"
+              variant="outlined"
+              onChange={(e) => setName(e.target.value)}
+              defaultValue={name}
+            />
+            <br></br>
+            <TextField
+              id="outlined-basic"
+              label="Movie Description"
+              variant="outlined"
+              onChange={(e) => setDescription(e.target.value)}
+              inputProps={{
+                maxLength: 50,
+              }}
+              defaultValue={description}
+            />
+            <br></br>
+            <TextField
+              id="outlined-basic"
+              label="Movie Cast"
+              variant="outlined"
+              onChange={(e) => setCast(e.target.value)}
+              defaultValue={cast}
+            />
+            <br></br>
+
+            <FormGroup>
+              <p id="label">Select Theaters:</p>
+
+              {theatersDB.map((theater) => {
+                return (
+                  <FormControlLabel
+                    key={theater._id}
+                    control={<Checkbox />}
+                    label={theater.theaterName}
+                    value={theater._id}
+                    onChange={(e) =>
+                      getSelectedTheaters(e.target.value, e.target.checked)
+                    }
+                  />
+                );
+              })}
+              <br></br>
+            </FormGroup>
+            <TextField
+              id="outlined-basic"
+              label="Show Time"
+              variant="outlined"
+              onChange={(e) => setShowTime(e.target.value)}
+              defaultValue={showTime}
+            />
+            <br></br>
+
+            <Button
+              variant="contained"
+              style={{ width: '400px', height: '40px' }}
+              onClick={onSubmit}
+            >
+              Submit
+            </Button>
+          </FormControl>{' '}
+        </Grid>
+      </Grid>
     </>
   );
 }
+
+//styling
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 50,
+    paddingRight: '100px',
+  },
+  preview: {
+    marginTop: 20,
+  },
+  image: { maxWidth: '100%', maxHeight: '500px' },
+  delete: {
+    cursor: 'pointer',
+    padding: 15,
+    background: 'red',
+    color: 'white',
+    border: 'none',
+  },
+};
